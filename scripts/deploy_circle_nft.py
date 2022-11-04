32b1bdb9ba42d2f4e95f49a21954396fdc40d2dd
danceratopz <danceratopz@gmail.com>
2022-11-04 07:54:22 +0100

    chore: check increase/decrease of radius in deploy script

--- scripts/deploy_circle_nft.py
+++ scripts/deploy_circle_nft.py
@@ -2,7 +2,34 @@ import json
 from urllib.request import urlopen
 
 import click
-from ape import accounts, networks, project
+from ape import accounts, convert, networks, project
+
+
+class CircleNFT:
+    def __init__(self, circle_nft, circle_token, owner):
+        self.circle_nft = circle_nft
+        self.circle_token = circle_token
+        self.address = circle_nft.address
+        self.owner = owner
+
+    def increase_radius(self, token_id, token_owner, amount):
+        self.circle_token.approve(self.address, amount, sender=token_owner)
+        self.circle_nft.increaseRadius(token_id, amount, sender=token_owner)
+
+    def decrease_radius(self, token_id, token_owner, amount):
+        self.circle_nft.decreaseRadius(token_id, amount, sender=token_owner)
+
+    def get_token_metadata(self, token_id):
+        token_uri = self.circle_nft.tokenURI(token_id)
+        with urlopen(token_uri) as f:
+            metadata = json.loads(f.read())
+        return metadata
+
+    def get_token_svg(self, token_id):
+        metadata = self.get_token_metadata(token_id)
+        with urlopen(metadata["image"]) as f:
+            svg = f.read()
+        return svg
 
 
 def main():
@@ -13,39 +40,58 @@ def main():
         f"You are connected to network '{ecosystem_name}:{network_name}:{provider_name}'."
     )
 
-    deployer = accounts.test_accounts[0]
-    account1 = accounts.test_accounts[1]
+    owner = accounts.test_accounts[0]
+    receiver = accounts.test_accounts[1]
 
-    circle_token = deployer.deploy(
-        project.ERC20Factory, "Circle Token", "CIRCLETOKEN", 100, account1.address
+    circle_token = owner.deploy(
+        project.ERC20Factory,
+        "Circle Token",
+        "CIRCLETOKEN",
+        convert("200 ETH", int),
+        receiver.address,
     )
     print(f"Gas used: {circle_token.receipt.gas_used}")
-    # circle_token_address = circle_token.address
-    print(circle_token.balanceOf(account1))
+    print(circle_token.balanceOf(receiver))
 
-    circle_nft = deployer.deploy(project.CircleNFT, circle_token.address)
+    circle_nft = owner.deploy(project.CircleNFT, circle_token.address)
     print(f"Gas used: {circle_nft.receipt.gas_used}")
 
-    receipt = circle_nft.mintNFT(account1, sender=deployer)
+    print("Minting circle...")
+    receipt = circle_nft.mintNFT(receiver, sender=owner)
     print(f"Gas used: {receipt.gas_used}")
-    token_uri = circle_nft.tokenURI(1)
 
-    with urlopen(token_uri) as response:
-        metadata = json.loads(response.read())
+    token_id = circle_nft.getLastTokenID()
+    circle_nft_helper = CircleNFT(circle_nft, circle_token, owner)
+    metadata = circle_nft_helper.get_token_metadata(token_id)
+    assert int(metadata["radius"]) == circle_nft.getRadius(token_id)
 
-    print(metadata)
-    print("radius", metadata["radius"])
+    new_radius = 50
+    print(f"\nIncreasing radius to {new_radius}...")
+    amount = new_radius - circle_nft.getRadius(token_id)
+    circle_nft_helper.increase_radius(token_id, receiver, amount)
+    metadata = circle_nft_helper.get_token_metadata(token_id)
+    assert int(metadata["radius"]) == circle_nft.getRadius(token_id)
+    assert int(metadata["radius"]) == new_radius
+    print(circle_nft_helper.get_token_svg(token_id))
 
-    circle_token.approve(circle_nft.address, 1**18, sender=account1)
-    circle_nft.increaseRadius(1, 1**18, sender=account1)
+    new_radius = 100
+    print(f"\nIncreasing radius to {new_radius}...")
+    amount = new_radius - circle_nft.getRadius(token_id)  # / 10**18
+    circle_nft_helper.increase_radius(token_id, receiver, amount)
+    metadata = circle_nft_helper.get_token_metadata(token_id)
+    assert int(metadata["radius"]) == circle_nft.getRadius(token_id)
+    assert int(metadata["radius"]) == new_radius
+    print(circle_nft_helper.get_token_svg(token_id))
 
-    token_uri = circle_nft.tokenURI(1)
-
-    with urlopen(token_uri) as response:
-        metadata = json.loads(response.read())
-
-    print(metadata)
-    print("radius", metadata["radius"])
+    new_radius = 75
+    print(f"\nDecreasing radius to {new_radius}...")
+    print(amount)
+    amount = circle_nft.getRadius(token_id) - new_radius  # / 10**18
+    circle_nft_helper.decrease_radius(token_id, receiver, amount)
+    metadata = circle_nft_helper.get_token_metadata(token_id)
+    assert int(metadata["radius"]) == circle_nft.getRadius(token_id)
+    assert int(metadata["radius"]) == new_radius
+    print(circle_nft_helper.get_token_svg(token_id))
 
     return circle_nft, circle_token
 
